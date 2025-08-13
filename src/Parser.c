878b3d2617a518d6d7f6bac3b3b338269c3405be
Fotis <tsipisf@gmail.com>
2025-08-14 00:07:37 +0300

    Working with the file managment

--- src/Parser.c
+++ src/Parser.c
@@ -7,8 +7,20 @@ void parser() {
     char ending[] = "_mapped.txt";                                             // The ending of the .txt                            
     char file[64], fullpath[512];                                              // File is the string needed for the parser e.g. s27
 
+    /*Creting the path for the .txt file*/
     printf("Type the starting string of the .txt file: ");
     scanf("%s", file);
     snprintf(fullpath, sizeof(fullpath), "%s%s%s", dir, file, ending);
     
+    FILE *fpointer = fopen(fullpath, "r");
+
+    char line[256];
+    char *word;
+
+    while (fgets(line, sizeof(line), fpointer)) {
+        word = strtok(line, " ");
+        if (!strcmp(word, "input")) {
+            printf("%s\n", word);
+        }
+    }
 }

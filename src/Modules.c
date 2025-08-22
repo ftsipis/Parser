0982f50581a92338d597e8ed317ed5cbb8327f27
Fotis <tsipisf@gmail.com>
2025-08-23 00:17:14 +0300

    Isolate the port name and formal port name of the gates

--- src/Modules.c
+++ src/Modules.c
@@ -53,18 +53,45 @@ struct Gate {
 
 /*Function for creation of Gate modules*/
 struct Gate *CreateGate (struct Gate *gate, char *type, char *name, char *inside, int number){
-    struct Gate *tmp = realloc(gate, number * sizeof *gate);
-    if (!tmp) {
+    struct Gate *test = realloc(gate, number * sizeof *gate);
+    if (!test) {
         perror("realloc failed");
         return gate; 
     }
 
-    gate = tmp;
+    gate = test;
     strcpy(gate[number-1].type, type);
     strcpy(gate[number-1].name, name);
 
     int inNumber = InputNumber(gate[number-1].type);
     gate[number-1].input = malloc(inNumber * sizeof(int));
+    
+    char *tmp = inside;
+    char *delim = " ,";
+    char *token = strtok(tmp, delim);
+    char *dot, *lpar, *rpar;
+    char formal[10], net[10];
+
+    while (token != NULL) {
+        if (strcmp(token, ")")) {
+            dot = strchr(token, '.');
+            lpar = strchr(token, '(');
+            rpar = strchr(token, ')');
+            if (dot && lpar && rpar && rpar > lpar) {
+                //copy formal port name (between '.' and '(')
+                size_t len1 = lpar - (dot + 1);
+                strncpy(formal, dot + 1, len1);
+                formal[len1] = '\0';
+
+                // copy net name (between '(' and ')')
+                size_t len2 = rpar - (lpar + 1);
+                strncpy(net, lpar + 1, len2);
+                net[len2] = '\0';                                           // Here I need to connect the IOs
+            }
+        }
+        token = strtok(NULL, delim);
+    }
+    printf("\n");
 
     return gate;
 };
@@ -83,7 +110,6 @@ int InputNumber (char *type) {
     int num;
     if (!strcmp(type, "DFF_X1") || !strcmp(type, "INV_X1")) {
         num = 1;
-        printf("Input Number = %d\n", num);
         return num;
     } else {
         for (char *p=type; *p; p++){
@@ -92,7 +118,6 @@ int InputNumber (char *type) {
                 break;
             }
         }
-        printf("Input Number = %d\n", num);
         return num;
     }
 }
